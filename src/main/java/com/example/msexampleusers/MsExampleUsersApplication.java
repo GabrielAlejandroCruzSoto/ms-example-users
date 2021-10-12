296ed7d1c2af17c9155089f24dd5b396d77ce36d
Gabriel Cruz Soto <gabrielcrsoto@outlook.com>
2021-10-12 19:16:47 -0300

    Project Initial MicroService Example Users

---
+++ src/main/java/com/example/msexampleusers/MsExampleUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.msexampleusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsExampleUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsExampleUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msexampleusers/MsExampleUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msexampleusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsExampleUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
